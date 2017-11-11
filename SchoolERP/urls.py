9ecac048cc5b6100cfc95c327ffe48d3304c1e08
IshanRD19 <ishanlsr@gmail.com>
2017-11-11 20:36:56 +0530

    personalinfo model

--- SchoolERP/urls.py
+++ SchoolERP/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^student/$', include('student.urls')),
+    url(r'^student/', include('student.urls')),
     #url(r'^teacher/', include('teacher.urls')),
     #url(r'^principal/', include('principal.urls'))
 ]
--- principal/models.py
+++ principal/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Personal_Info(models.Model):
+    Index = models.IntegerField(primary_key=True)
+    Role = models.CharField(max_length=20)
+    FirstName = models.CharField(max_length=20)
+    MiddleName = models.CharField(max_length=20)
+    LastName = models.CharField(max_length=20)
+    DOB = models.DateField(auto_now=False)
+    Sex = models.CharField(max_length=12)
+    Contact = models.CharField(max_length=12)
+    EMail = models.CharField(max_length=25)
+    BloodGroup = models.CharField(max_length=3)
+    AddressLine1 = models.TextField(max_length=50)
+    AddressLine2 = models.CharField(max_length=50)
+    AddressLine3 = models.CharField(max_length=50)
+    Pincode = models.CharField(max_length=6)
+    CommuteDistance = models.IntegerField()             # XX.X kms
+    CommuteTime = models.IntegerField()                 # minutes
+    CommuteBy = models.CharField(max_length=10)
