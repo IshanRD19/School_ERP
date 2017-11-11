032dbcab1d14cc4db5d472be02210266cf14c441
IshanRD19 <ishanlsr@gmail.com>
2017-11-11 21:26:17 +0530

    principal model

--- principal/models.py
+++ principal/models.py
@@ -20,3 +20,34 @@ class Personal_Info(models.Model):
     CommuteDistance = models.IntegerField()             # XX.X kms
     CommuteTime = models.IntegerField()                 # minutes
     CommuteBy = models.CharField(max_length=10)
+
+
+class Rooms_2018(models.Model):
+    RoomID = models.CharField(primary_key=True, max_length=8)
+    Capacity = models.IntegerField(default=0)
+    Floor = models.IntegerField(default=0)
+    Fans = models.IntegerField(default=0)
+    Lights = models.IntegerField(default=0)
+    AC = models.IntegerField(default=0)
+    Windows = models.IntegerField(default=0)
+    Doors = models.IntegerField(default=0)
+
+
+class Subjects(models.Model):
+    SubjectCode = models.CharField(primary_key=True, max_length=5)          # 07GEO, 12MAT
+    SubjectName = models.CharField(max_length=20)                           # GEO, MAT
+    TaughtSince = models.DateField(auto_now=True)
+    TaughtTill = models.DateField(auto_now=True)
+    NoOfTeachers = models.IntegerField(default=0)                        # total in all classes and sections
+
+
+class Feedback(models.Model):
+    FeedbackID = models.CharField(primary_key=True, max_length=10)
+    GivenBy = models.CharField(max_length=20)
+    GivenFor = models.CharField(max_length=20)
+    SubjectCode = models.ForeignKey(Subjects, max_length=5)
+    SubjectDifficulty = models.IntegerField(default=0)                   # rating of 1 to 5
+    CreateInterest = models.IntegerField(default=0)
+    HomeWorkLoad = models.IntegerField(default=0)
+    OverallPerformance = models.IntegerField(default=0)
+    Suggestions = models.TextField(max_length=50, default='NA')
