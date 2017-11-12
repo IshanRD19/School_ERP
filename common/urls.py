fe15351fc1648bc6292425f192aa97a178b07ab7
A Bhaskar Chary <abhaskarchary@gmail.com>
2017-11-12 10:03:31 +0530

    principal view

--- common/urls.py
+++ common/urls.py
@@ -13,14 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    #homepage url
-    url(r'^', include('common.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^student/', include('student.urls')),
-    #url(r'^teacher/', include('teacher.urls')),
-    #url(r'^principal/', include('principal.urls'))
+    # homepage url
+    url(r'^$', views.home, name='home'),
+    url(r'^login/$', views.login_attempt, name='login_attempt'),
 ]
--- principal/admin.py
+++ principal/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import login_info, Personal_Info
 # Register your models here.
+admin.site.register(login_info)
+admin.site.register(Personal_Info)
