d86a3d120250f14d93b4108998590196ec859652
IshanRD19 <ishanlsr@gmail.com>
2017-11-12 10:49:53 +0530

    teacher model

--- student/models.py
+++ student/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from principal.models import Personal_Info, Subjects
-
+from teacher.models import Teachers_2018, Classes_2018
 # Create your models here.
 
 class Students_2018(models.Model):
     RegistrationNo = models.CharField(primary_key=True, max_length=20)
     Index = models.ForeignKey(Personal_Info)
-    ClassSection = models.CharField(max_length=3, default='NA')             # 07B
+    ClassSection = models.ForeignKey(Classes_2018, max_length=3, default='NA') # 07B
     RollNo = models.CharField(max_length=3, default='NA')
     From = models.DateField(auto_now=False)                                 # active from date
     Till = models.DateField(auto_now=False)                                 # active till date
@@ -15,22 +15,27 @@ class Students_2018(models.Model):
     ClassRank = models.IntegerField(default=0)                              # current rank in class
 
 
-class TimeTableLookUp(model.Model):
+class TimeTableLookUp(models.Model):
     TTID = models.CharField(primary_key=True, max_length=3)                 # Numerals 1,2,3,...
     WeekDay = models.CharField(max_length=9)                                # Monday,Tuesday,...
+    ClassSection = models.CharField(max_length=3, default='NA')
     StartTime = models.TimeField()
     EndTime = models.TimeField()
 
 
 class TimeTable_2018(models.Model):
     TTID = models.ForeignKey(TimeTableLookUp, primary_key=True)
-    ClassSection = models.CharField(primary_key=True, max_length=3)
     SubjectCode = models.ForeignKey(Subjects)
-    #TeacherID = models.ForeignKey(Teachers_2018)
+    TeacherID = models.ForeignKey(Teachers_2018)
+
+
+class Attendance2018LookUp(models.Model):
+    AttendanceID = models.CharField(primary_key=True, max_length=20)       # regno/09C-48-DDMMYYYY-TTID
+    RegistrationNo = models.ForeignKey(Students_2018)
+    ClassDate = models.DateField(auto_now=False)                           # DD/MM/YYYY
+    TTID = models.ForeignKey(TimeTableLookUp)
 
 
 class Attendance_2018(models.Model):
-    RegistrationNo = models.ForeignKey(Students_2018, primary_key=True)
-    TTID = models.ForeignKey(TimeTableLookUp, primary_key=True, max_length=12)
-    ClassDate = models.DateField(primary_key=True, auto_now=False)         # DD/MM/YYYY
+    AttendanceID = models.ForeignKey(Attendance2018LookUp, primary_key=True, unique=True)
     Attendance = models.CharField(max_length=8)                            # present or absent
--- teacher/models.py
+++ teacher/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from principal.models import  Personal_Info, Rooms_2018, Subjects
 # Create your models here.
+
+class Teachers_2018(models.Model):
+    TeacherID = models.CharField(primary_key=True, max_length=12)
+    Index = models.ForeignKey(Personal_Info)
+    ClassesPerWeek = models.IntegerField(default=0)
+    Qualification = models.TextField(max_length=50, default='NA')
+    Experience = models.IntegerField(default=0)                             # no. of months
+    Salary = models.IntegerField(default=0)                                 # without commas
+
+
+class Classes_2018(models.Model):
+    ClassSection = models.CharField(primary_key=True, max_length=3)         # 09C
+    RoomID = models.ForeignKey(Rooms_2018)
+    ClassTeacherID = models.ForeignKey(Teachers_2018)
+    Students = models.IntegerField(default=0)
+    Subjects = models.IntegerField(default=0)
+    From = models.DateField(auto_now=False)
+    Till = models.DateField(auto_now=False)
+
+
+class Q_Papers(models.Model):
+    QPaperID = models.CharField(primary_key=True, max_length=15)            # DDMMYYYY-09C-GEO-2
+    SubjectCode = models.ForeignKey(Subjects)
+    MadeBy = models.ForeignKey(Teachers_2018)
+    Topic = models.TextField(max_length=25)                                 # test of which unit or topic
+    MaxMarks = models.IntegerField(default=0)
+    StartTime = models.TimeField()
+    EndTime = models.TimeField()
+    Appeared = models.IntegerField(default=0)                               # total students who took test
+    AvgMarks = models.IntegerField(default=0)
