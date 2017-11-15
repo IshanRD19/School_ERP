ed854349c619bed361ae62528e7eb32a4a0d0956
IshanRD19 <ishanlsr@gmail.com>
2017-11-15 13:30:27 +0530

    migration fix

--- principal/admin.py
+++ principal/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import login_info, Personal_Info, Subjects, Rooms_2018
+from .models import *
 # Register your models here.
 admin.site.register(login_info)
 admin.site.register(Personal_Info)
 admin.site.register(Subjects)
 admin.site.register(Rooms_2018)
+admin.site.register(AssignmentParameters)
